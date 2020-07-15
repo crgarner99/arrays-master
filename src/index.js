a41177bcf74e87895689d059bd49e92bd45e9efc
Cameron Garner <cameron.r.garner@gmail.com>
2020-07-14 20:41:31 -0600

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,88 @@
-import Senators from './data/senators'
+import Senators from "./data/senators";
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const republicans = Senators.filter((senator) => {
+    if (senator.party === "Republican") {
+      return true;
+    }
+  });
+  return republicans;
+};
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const democrats = Senators.filter((senator) => {
+    if (senator.party === "Democrat") {
+      return true;
+    }
+  });
+  return democrats;
+};
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const independents = Senators.filter((senator) => {
+    if (senator.party === "Independent") {
+      return true;
+    }
+  });
+  return independents;
+};
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const males = Senators.filter((senator) => {
+    if (senator.person.gender_label === "Male") {
+      return true;
+    }
+  });
+  return males;
+};
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const females = Senators.filter((senator) => {
+    if (senator.person.gender_label === "Female") {
+      return true;
+    }
+  });
+  return females;
+};
 
-export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
-}
+export const byState = (state = "UT") => {
+  const byState = Senators.filter((senator) => {
+    if (senator.state === "UT") {
+      return true;
+    }
+  });
+  return byState;
+};
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const senatorMapping = Senators.map((senator) => {
+    const newSenator = {
+      firstName: senator.person.firstname,
+      lastName: senator.person.lastname,
+      party: senator.party,
+      gender: senator.person.gender,
+    };
+    return newSenator;
+  });
+
+  return senatorMapping;
+};
 
 export const reducedCount = () => {
-  return REPLACE_ME_WITH_CODE
-}
+  const partyCounts = Senators.reduce(
+    (accum, senator) => {
+      if (senator.party === "Republican") {
+        accum.republican++;
+      } else if (senator.party === "Democrat") {
+        accum.democrat++;
+      } else {
+        accum.independent++;
+      }
+      return accum;
+    },
+    { republican: 0, democrat: 0, independent: 0 }
+  );
+  return partyCounts;
+};
 
-
-const REPLACE_ME_WITH_CODE = false
+const REPLACE_ME_WITH_CODE = false;
